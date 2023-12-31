559c4ee0aec3c583a96be38e82a2e8bd3ddca8f8
denlove <bronzovdenis59@gmail.com>
2023-12-31 05:21:49 +0300

    DD-6: add new minor fixes

--- app/django_docker/settings.py
+++ app/django_docker/settings.py
@@ -122,9 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ["82.97.241.5"]
