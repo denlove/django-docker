d6377e82048e191992bb95f08a9a9580bc7aa9e4
denlove <bronzovdenis59@gmail.com>
2023-12-31 16:54:07 +0300

    DD-7: again fucking fixed

--- app/django_docker/urls.py
+++ app/django_docker/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("projectApp.urls")),
+    # path("", include("projectApp.urls")),
 ]
-
